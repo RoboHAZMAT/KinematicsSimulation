c3afcd482652ff3d6e14ff9d4a8538c09dfbfedf
gbledt <gbledt10@vt.edu>
2014-12-16 02:25:50 -0500

    Dramatic increase to optimization
    Speeds up the optimization for the inverse kinematics to almost
    real-time.

--- TrajectoryPlanningOptimization/optimize.m
+++ TrajectoryPlanningOptimization/optimize.m
@@ -1,5 +1,9 @@
 function X = optimize(robot, name, pts)
-tic
+% Optimization function that takes in a robotic system, the name of the
+% kinematic chain to move around, and the points to move the frame points
+% to and uses fmincon to move the system around.
+
+
 KC = EmptyKinematics();
 fields = fieldnames(robot.KinematicChains);
 
@@ -10,7 +14,7 @@ for i = 1:length(fields)
 end
 X0 = KC.DHParams.thetas - KC.thetas.thi;
 options = optimset('Algorithm','interior-point','Display','off',...
-    'TolFun',1e-2,'DiffMinChange',1/180*pi);
+    'TolFun',1e-2,'DiffMinChange',2/180*pi);
+
 X = fmincon(@(X)errorFunc(X,KC,pts),X0,[],[],[],[],...
     KC.bounds.lb,KC.bounds.ub,[],options);
-toc
