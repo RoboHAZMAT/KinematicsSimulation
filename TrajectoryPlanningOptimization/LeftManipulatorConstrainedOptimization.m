3d46d47b8b3de312c859b8c5161a815550814186
gbledt <gbledt10@vt.edu>
2014-12-24 20:26:30 -0500

    Clean up unused programs

--- TrajectoryPlanningOptimization/LeftManipulatorConstrainedOptimization.m
+++
@@ -1,6 +0,0 @@
-function X = LeftManipulatorConstrainedOptimization(Xd, X0)
-X0 = [0,0,0,0,0,0,0];
-upperBounds = [pi,pi,pi,pi,pi,pi,pi,pi];
-lowerBounds = [-pi,-pi,-pi,-pi,-pi,-pi,-pi,-pi];
-options = optimset('Algorithm','interior-point');
-X = fmincon(@errorFunc,X0,[],[],[],[],lowerBounds,upperBounds,[],options);
