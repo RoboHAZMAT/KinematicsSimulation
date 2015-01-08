f25d4548eb5f5d99f499bf798e2f01d25828496b
gbledt <gbledt10@vt.edu>
2015-01-08 18:14:00 -0500

    Adds Status To Sims

--- Simulations/MechatronicArm/SimulationM3.m
+++ Simulations/MechatronicArm/SimulationM3.m
@@ -8,10 +8,12 @@ function Robot = SimulationM3(Robot)
 [IMUCOM, motorControlCOM] = SetupCOM;
 [serialMotorControl, motor] = ...
     SetupMotorControlSerial(motorControlCOM);
+clc;
 
 % Sets up the kinematics
+controlPoint = 6;
 KC = Robot.KinematicChains.MAK;
-KC.optimization.weightings(5) = 100;
+KC.optimization.weightings(controlPoint) = 100;
 pointsd = zeros(4, size(KC.points.p,2));
 
 % History vectors for trajectories
@@ -23,40 +25,31 @@ yHistT = zeros(1,360);
 zHistT = zeros(1,360);
 
 % Trajectory parameters
-traj = 3;
-velocity = 5; % Set velocity: {1,..,.5}
-runs = 3;     % Number of runs
-trajDur = 60; % Trajectory segment duration
-n = 6;        % 6 for a full 360 cycle
+traj = 4;
+velocity = 1; % Set velocity: {1,..,.5}
+trajDur = 1;  % Trajectory segment duration
+n = 15;       % 13 for a full cycle
+count = zeros(3,1);
+
+% Option parameters
+varsin.trajDur = trajDur;
+varsin.serialMotorControl = serialMotorControl;
+varsin.motor = motor;
+
+% Open gripper to start
+serialMotorControl.servoWrite(motor.gripper, 0);
+pause(0.5);
 
 while (states.run)
-    % Get current states
-    states = guidata(RobotFigure);
-    
-    serialMotorControl.servoWrite(motor.gripper, 0);
-    pause(0.5);
     % Runs the trajectory multiple times
-    for i = 1:velocity:(n*runs*trajDur - 1)
+    for i = 1:velocity:(n*trajDur - 1)
         
-        % Open and close the gripper
-        if (mod(i-1, n) == 0)
-            if (i-1 == n)
-                serialMotorControl.servoWrite(motor.gripper, 180);
-                pause(0.5);
-            elseif (i-1 == 4*n)
-                serialMotorControl.servoWrite(motor.gripper, 0);
-                pause(0.5);
-            elseif (i-1 == 8*n)
-                serialMotorControl.servoWrite(motor.gripper, 180);
-                pause(0.5);
-            elseif (i-1 == 11*n)
-                serialMotorControl.servoWrite(motor.gripper, 0);
-                pause(0.5);
-            end
-        end
+        % Get current states
+        states = guidata(RobotFigure);
+        if (~states.run), break; end;
         
         % Create the trajectory
-        [x, y, z] = TrajectoriesMechatronicArm(i, traj, trajDur);
+        [x, y, z] = TrajectoriesMechatronicArm(i, traj, varsin);
         
         % Assigns the desired trajectory history
         xHist(i) = x;
@@ -65,7 +58,7 @@ while (states.run)
         
         % Adds noise to trajectory and carries out inverse
         % kinematics on the noisy trajectory
-        pointsd(:,size(KC.points.p,2)) = [x; y; z; 1];
+        pointsd(:,controlPoint) = [x; y; z; 1];
         X = InverseKinematicOptimization(KC,pointsd);
         
         % Rotates and plots the right arm to optimized value
@@ -82,6 +75,15 @@ while (states.run)
         % plot3(xHistT,yHistT,zHistT,'.','color','red');
         drawnow;
         
+        % Move arm servos
         MechatronicArmControl(serialMotorControl, motor, X);
+        
+        X = X*180/pi;
+        fprintf(1, repmat('\b',1,sum(count)));
+        count(1) = fprintf('  Base Yaw   Base Pitch   Elbow Pitch   Wrist Pitch\n');
+        count(2) = fprintf('%9.3f%12.3f%14.3f%14.3f\n\n',...
+            X(1),X(2),X(3),X(4));
+        count(3) = fprintf('    X: %.3f           Y: %.3f            Z: %.3f\n',...
+            KC.points.pG(1,5),KC.points.pG(2,5),KC.points.pG(3,5));
     end
 end
--- Simulations/MechatronicArm/SimulationM4.m
+++ Simulations/MechatronicArm/SimulationM4.m
@@ -1,14 +1,16 @@
 function Robot = SimulationM4(Robot)
-%% =================Keyboard Controlled Mechatronic Arm====================
+%% ===========Keyboard Controlled Mechatronic Arm Simulation===============
 
 % Initializes the arm states and setp size
 [RobotFigure, states] = SetupKeyboardControl(Robot);
 
 % Sets up the kinematics for the arm
-controlPoint = 5;
+controlPoint = 6;
 KC = Robot.KinematicChains.MAK;
 KC.optimization.weightings(controlPoint) = 100;
 pointsd = zeros(4, size(KC.points.kP,2));
+
+count = [0;0;0];
 
 % Runs the loop a given number of times
 while(states.run)
@@ -22,9 +24,19 @@ while(states.run)
     X = InverseKinematicOptimization(KC,pointsd);
     % Rotates the KC
     KC = RotateKinematicChain(KC,X);
+    KC = RotateKinematicPoints(KC,X);
     Robot.KinematicChains.MAK = KC;
     
     % Plots the Robot
     RobotPlot(Robot);
     drawnow;
+    
+    X = X*180/pi;
+    fprintf(1, repmat('\b',1,sum(count)));
+    count(1) = fprintf('  Base Yaw   Base Pitch   Elbow Pitch   Wrist Pitch\n');
+    count(2) = fprintf('%9.3f%12.3f%14.3f%14.3f\n\n',...
+        X(1),X(2),X(3),X(4));
+    count(3) = fprintf('  X: %.3f           Y: %.3f            Z: %.3f\n',...
+        KC.points.kPG(1,controlPoint),KC.points.kPG(2,controlPoint),...
+        KC.points.kPG(3,controlPoint));
 end
