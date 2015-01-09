487e387a9bfd3728e426e9cf5e8ee750f196d3ef
gbledt <gbledt10@vt.edu>
2015-01-09 18:10:34 -0500

    RoboHAZMAT Improvements

--- Simulations/RoboHAZMAT/Simulation1.m
+++ Simulations/RoboHAZMAT/Simulation1.m
@@ -22,22 +22,24 @@ for i = 1:length(fields)
     end
 end
 
-controlPoint = 6;
-KC.optimization.weightings(controlPoint) = 10;
-
 % If the robot has the kinematic chain, continue
 if (valid)
     fprintf([KCstring,' was selected\n']);
+    
+    fprintf('\nChoose point to control. Select {1,...,%i}\n',size(KC.points.p,2));
+    controlPoint = input(' Control Point = ');
+    KC.optimization.weightings(controlPoint) = 100;
+    
     fprintf('\nChoose (x, y, z) location to move the gripper to.\n');
     
     % Input an x, y, z location to move the kinematic chain to
-    x = input(' x = ');
-    y = input(' y = ');
-    z = input(' z = ');
+    traj.point(1,1) = input(' x = ');
+    traj.point(2,1) = input(' y = ');
+    traj.point(3,1) = input(' z = ');
     
     % Inverse kinematics
     pointsd = [zeros(3, size(KC.points.kP,2));ones(1,size(KC.points.kP,2))];
-    pointsd(:,controlPoint) = [x;y;z;1];
+    pointsd(:,controlPoint) = [traj.point;1];
     X = InverseKinematicOptimization(KC, pointsd);
     
     % Rotates and plots the kinematic chain
