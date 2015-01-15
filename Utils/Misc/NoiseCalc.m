91b97bc9c1ed3dfeee6121601f26e5217deeb2cc
gbledt <gbledt10@vt.edu>
2015-01-15 03:38:26 -0500

    Finishes Documentation

--- Utils/Misc/NoiseCalc.m
+++ Utils/Misc/NoiseCalc.m
@@ -1,7 +1,20 @@
 function noise = NoiseCalc
+%% ==========================Noise Calculation=============================
+% RoboHAZMAT: Senior Design Project
+% Motion Control Team
+% Gerardo Bledt
+% December 15, 2014
+%
+% Calculates synthetic noise to be used in a generic situation. Magnitude
+% can be changed to fit various applications.
 
+% The magnitude of the noise
 mag = 0.005;
+
+% Creates the Gaussian PDF
 noisePDF = normpdf(-1:0.001:1,0,.75);
+
+% Cooses random point on the distribution
 index = randi(length(noisePDF));
 % noise = mag*(noisePDF(index)/max(noisePDF));
 if (index > length(noisePDF)/2)
--- Utils/Misc/PrintStatusReport.m
+++ Utils/Misc/PrintStatusReport.m
@@ -1,11 +1,22 @@
 function status = PrintStatusReport(KC, X, status)
+%% =========================Print Status Report============================
+% RoboHAZMAT: Senior Design Project
+% Motion Control Team
+% Gerardo Bledt
+% January 10, 2015
+%
+% Prints a status report for the Kinematic System. Allows the user to
+% monitor the degrees of freedom and the location of any requested points.
 
+% Prints the joint angles for the thetas
 X = X*180/pi;
 fprintf(1, repmat('\b',1,status.count));
 status.count = fprintf('  Base Yaw   Base Pitch   Elbow Pitch   Wrist Pitch\n');
 status.count = status.count + fprintf('%9.3f%12.3f%14.3f%14.3f\n',...
     X(1),X(2),X(3),X(4));
 
+
+% Prints the location of the requested points in 3D
 status.count = status.count + fprintf('=====================================================\n');
 for i = 1:size(status.point,1);
     status.count = status.count + fprintf(' %i:   X: %7.2f        Y: %7.2f       Z: %7.2f\n',...
